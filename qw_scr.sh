d00b96609437b783e47e012b21bbc6c1c9ba0de5
Qingyun Wu <qw2ky@virginia.edu>
2021-03-16 19:42:04 -0700

    update gitingore

--- qw_scr.sh
+++ qw_scr.sh
@@ -1,77 +1,127 @@
-# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna' -r  5 
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 6
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  7
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 8
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  9
-# sleep 10s
+# git fetch upstream
+# git merge xxx
 
-# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA' -r  5
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA' -r 6
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA' -r  7
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA'  -r 8
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA'  -r  9
-# sleep 10s
+# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  'BlendSearch+Optuna+fixedepo' -r  0  -plot_only
 
-# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna' -r  5
+# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  0 
 # sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 6
+# screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 1
 # sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  7
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  2
 # sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 8
+# screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 3
 # sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  9
-# sleep 10s
-
-# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA' -r  5
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA' -r 6
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA' -r  7
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA'  -r 8
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA'  -r  9
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  4
 # sleep 10s
 
 
-
-# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna' -r  0 
+# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo' -r  0 
 # sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 1
+# screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 1
 # sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  2
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  2
 # sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 3
+# screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 3
 # sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  4
-# sleep 10s
-
-
-# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna' -r  0
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 1
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  2
-# sleep 10s
-# screen -Sdm Optuna python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r 3
-# sleep 10s
-# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna'  -r  4
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  4
 # sleep 10s
 
+# sleep 2000s
+# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo' -r  0
+# sleep 10s
+# screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 1
+# sleep 10s
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  2
+# sleep 10s
+# screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 3
+# sleep 10s
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  4
+# sleep 10s
 
-screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna' -r  0 
+
+# screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo' -r  0
+# sleep 10s
+# screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 1
+# sleep 10s
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  2
+# sleep 10s
+# screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 3
+# sleep 10s
+# screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  4
+# sleep 10s
+
+screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r  5
 sleep 10s
-screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+ASHA' -r  0 
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r 6
 sleep 10s
-screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 8  -m 'BlendSearch+Optuna' -r  0 
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r 7
 sleep 10s
-screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 8  -m 'BlendSearch+Optuna+ASHA' -r  0 
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r 8
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r  9
+sleep 10s
+
+screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  5
+sleep 10s
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 6
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 7
+sleep 10s
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 8
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  9
+sleep 10s
+
+sleep 2000s
+screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo' -r  5 
+sleep 10s
+screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 6
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  7
+sleep 10s
+screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 8
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  9
+sleep 10s
+
+
+screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r  5
+sleep 10s
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r 6
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r 7
+sleep 10s
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r 8
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r  9
+sleep 10s
+
+sleep 2000s
+screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo' -r  5
+sleep 10s
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 6
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  7
+sleep 10s
+screen -Sdm Optuna+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r 8
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'Optuna+fixedepo'  -r  9 
+sleep 10s
+
+
+screen -Sdm BS python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo' -r  5
+sleep 10s
+screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 6
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  7
+sleep 10s
+screen -Sdm CFO+fixedepo python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r 8
+sleep 10s
+screen -Sdm ASHA python test/test_dt_parallel.py  -t 1800 -d 'vehicle' -trial_pu 2 -total_pu 2  -m 'CFO+fixedepo'  -r  9
+sleep 10s
+
+python test/test_dt_parallel.py  -t 1800 -d 'phoneme' -trial_pu 2 -total_pu 2  -m 'BlendSearch+Optuna+fixedepo'  -r  5
+
+
+
+python test/test_automl_exp.py  -t 600 -trial_pu 2  -total_pu 2  -m 'BlendSearch+Optuna' -r 0 -d 'phoneme'
